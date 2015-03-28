9da58805e5b48a97ba943601100565eecc3f96e7
zach pratt <zach@attackofzach.com>
2015-03-27 23:36:45 -0500

    Add a google maps mixin, which can be used to asynchronously load the google maps api by a component that is dependent upon the google maps api.

---
+++ test/google-maps-mixin.spec.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var GoogleMapsMixin = require('../lib/google-maps-mixin'),
+
+    MapLoader = require('async-google-maps').MapLoader,
+
+    React = require('react'),
+    ReactTestUtils = require('react/lib/ReactTestUtils'),
+
+    sinon = require('sinon'),
+
+    expectedApiKey,
+    expectedApiVersion,
+    ComponentWithMixin,
+    ElementWithMixin,
+    sandbox;
+
+describe('Test Suite', function () {
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+
+        sandbox.stub(MapLoader, 'load');
+
+        ComponentWithMixin = React.createClass({
+            mixins: [GoogleMapsMixin],
+
+            render: function () {
+                return React.DOM.div();
+            }
+        });
+
+        expectedApiKey = 'somekey';
+        expectedApiVersion = 'someversion';
+
+        ElementWithMixin = React.createElement(ComponentWithMixin, {
+            apiKey: expectedApiKey,
+            apiVersion: expectedApiVersion
+        });
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    it('should load the map with the provided api key and version', function () {
+        sinon.assert.notCalled(MapLoader.load);
+
+        ReactTestUtils.renderIntoDocument(ElementWithMixin);
+
+        sinon.assert.calledOnce(MapLoader.load);
+        sinon.assert.calledWith(MapLoader.load, {
+            key: expectedApiKey,
+            version: expectedApiVersion
+        });
+    });
+});
